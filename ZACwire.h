447cdde58a0201f472441af1c45ee480570a5530
Tobias Sandhaas <21081031+medlor@users.noreply.github.com>
2021-09-11 21:28:06 +0200

    Update ZACwire.h

--- ZACwire.h
+++ ZACwire.h
@@ -1,6 +1,6 @@
 /*	ZACwire - Library for reading temperature sensors TSIC 206/306/506
 	created by Adrian Immer in 2020
-	v1.3.3
+	temporary fixed by medlor: v1.3.1 code put into 1.3.3 base 
 */
 
 #ifndef ZACwire_h
@@ -23,7 +23,7 @@ class ZACwire {
 	public:
 
 		ZACwire(int sensor = 306, byte defaultBitWindow=125, bool core=1) {
-			_sensor = sensor;
+			_Sensortype = sensor;
 			bitWindow = defaultBitWindow + (defaultBitWindow>>2);		//expected BitWindow in µs, depends on sensor & temperature
 			_core = core;							//only ESP32: choose cpu0 or cpu1
 		}
@@ -31,10 +31,10 @@ class ZACwire {
 		bool begin() {								//start collecting data, needs to be called 100+ms before first getTemp()
 			pinMode(pin, INPUT);
 			if (!pulseInLong(pin, LOW)) return false;			//check if there is incoming signal
-			byte isrPin = digitalPinToInterrupt(pin);
+			isrPin = digitalPinToInterrupt(pin);
 			if (isrPin == 255) return false;
 			#ifdef ESP32
-			xTaskCreatePinnedToCore(attachISR_ESP32,"attachISR",2000,NULL,1,NULL,_core); //freeRTOS
+			xTaskCreatePinnedToCore(attachISR_ESP32,"attachISR",2000,NULL,1,NULL,_core); //freeRTOS^
 			#elif defined(ARDUINO_ESP8266_RELEASE_)				//In ARDUINO_ESP8266_RELEASE_3.0.0 a new version of gcc with bug 70435
 			ETS_GPIO_INTR_ATTACH(tsicIsrHandler,(intptr_t)isrPin);		//...is included, which has issues with IRAM and template classes.
 			gpio_pin_intr_state_set(isrPin,GPIO_PIN_INTR_POSEDGE);		//...That's the reason to use nonOS here
@@ -45,43 +45,40 @@ class ZACwire {
 			return true;
 		}
 	  
-		float getTemp() {							//return temperature in °C
-			static unsigned int lastHB;
-			if (!heartbeat) {						//check wire connection
-				if (!bitCounter) {					//use bitCounter to prove that ISR was already attached
-					begin();
-					delay(110);
-				}
-				else if (!lastHB) lastHB = millis();			//record first missing heartbeat
-				else if ((unsigned int)millis() - lastHB > 255) return 221;	//return error 221 after timeout of 255ms
+	float getTemp() {								//give back temperature in °C
+		static bool misreading;
+		uint16_t temp[2];
+		if ((unsigned int)millis() - lastISR > 255) {	//check wire connection for the last 255ms
+			if (bitWindow) return 221;				// temp=221 if sensor not connected
+			else {									// w/o bitWindow, begin() wasn't called before
+				begin();
+				delay(110);
 			}
-			else {
-				lastHB = 0;
-				if (bitCounter > 4 && bitCounter < 11) {		//adjust bitWindow
-					byte newBitWindow = rawData[backUP] >> (bitCounter - 2);	//seperate newBitWindow from temperature bits and divide by 4
-					newBitWindow = (((newBitWindow>>1) + newBitWindow) >> 1) + 4 + (bitWindow>>2);	//divide by 1.31 (w/ previous line: by 5.25) and add 1/4 bitWindow
-					bitWindow < newBitWindow ? ++bitWindow : --bitWindow;
-				}
-			}
-			
-			static bool useBackup;
-			if (bitCounter != 19) useBackup = true;				//when newer reading is incomplete
-			uint16_t temp = rawData[backUP^useBackup];			//get rawData from ISR
-			
-			if (tempCheck(temp)) {
-				temp >>= 1;						//delete second parity bit
-				temp = (temp >> 2 & 1792) | (temp & 255);		//delete first    "     "
-				static int prevTemp = temp;
-				int grad ((temp - prevTemp) / (heartbeat|1));		//grad is [°C/s]
-				if (abs(grad) < 20) {					//limit change rate to 20°C/s to detect misreadings
-					prevTemp = temp;
-					heartbeat = useBackup = 0;
-					return (_sensor < 400 ? (temp * 250L >> 8) - 499 : (temp * 175L >> 9) - 99) / 10.0;	//use different formula for 206&306 or 506
-				}
-			}
-			useBackup = !useBackup;						//reset useBackup after use
-			return useBackup ? getTemp(): 222;				//restart with backUP rawData or return error value 222
 		}
+		if (BitCounter == 19) {
+			byte newBitWindow = ((ByteTime << 1) + ByteTime >> 5) + (range >> 1);  //divide by around 10.5 and add half range
+			bitWindow < newBitWindow ? ++bitWindow : --bitWindow;	//adjust bitWindow
+		}
+		else misreading = true;						//use misreading-backup when newer reading is incomplete
+		bool _backUP = backUP^misreading;
+		temp[0] = rawTemp[0][_backUP];				//get high significant bits from ISR
+		temp[1] = rawTemp[1][_backUP];				//get low   ''    ''
+		misreading = !misreading;					//reset misreading after use
+		
+		bool parity = true;
+		for (byte j=0; j<2; ++j) {
+			for (byte i=0; i<9; ++i) parity ^= temp[j] & 1 << i;	//check parity
+			temp[j] >>= 1;							//delete parity bit
+		}
+		if (parity) { 								//check for failure
+			temp[1] |= temp[0] << 8;				//join high and low significant figures
+			misreading = false;
+			if (_Sensortype < 400) return float((temp[1] * 250L >> 8) - 499) / 10;  //calculate °C
+			else return float((temp[1] * 175L >> 9) - 99) / 10;
+		}
+		else if (misreading) return getTemp();		//restart with backUP raw temperature
+		return 222;									//temp=222 if reading failed
+	}
 
 		void end() {
 			detachInterrupt(digitalPinToInterrupt(pin));
@@ -90,12 +87,12 @@ class ZACwire {
 	private:
 
 		#ifdef ESP32
-		static void attachISR_ESP32(void*){					//attach ISR in freeRTOS because gcc 5.3+ can't put ISR in IRAM inside of template class
-			gpio_num_t isrPin = (gpio_num_t)pin;
-			gpio_pad_select_gpio(isrPin);
-			gpio_set_intr_type(isrPin, GPIO_INTR_POSEDGE);
+		static void attachISR_ESP32(void *arg){			//attach ISR in freeRTOS because arduino can't attachInterrupt() inside of template class
+			gpio_pad_select_gpio((gpio_num_t)isrPin);
+			gpio_set_direction((gpio_num_t)isrPin, GPIO_MODE_INPUT);  //TOBI NEW
+			gpio_set_intr_type((gpio_num_t)isrPin, GPIO_INTR_POSEDGE);
 			gpio_install_isr_service(0);
-			gpio_isr_handler_add(isrPin, read, NULL);
+			gpio_isr_handler_add((gpio_num_t)isrPin, read, NULL);
 			vTaskDelete(NULL);
 		}
 		static void IRAM_ATTR read(void*) {
@@ -109,54 +106,56 @@ class ZACwire {
 		#else
 		static void read() {							//get called with every rising edge
 		#endif
-			if (++bitCounter > 4) {						//first 4 bits always =0
+			if (++BitCounter > 4) {						//first 4 bits always =0
+				static bool ByteNr;
 				unsigned int microtime = micros();
 				static unsigned int deltaTime;
-				deltaTime = microtime - deltaTime;			//measure time to previous rising edge
-				if (deltaTime >> 10) {					//start bit
-					backUP ^= bitCounter == 20;
-					bitCounter = 0;
-					++heartbeat;					//give a sign of life to getTemp()
+				deltaTime = microtime - deltaTime;		//measure time to previous rising edge
+				if (deltaTime >> 10) {					//true at start bit
+					ByteTime = microtime;				//for measuring Tstrobe/bitWindow
+					backUP = !backUP;
+					BitCounter = 0;
+					lastISR = millis();					//for checking wire connection
 				}
-				else if (bitCounter == 5) rawData[backUP] = deltaTime<<1 | 2;	//send deltaTime for calculating bitWindow and add prefix "10" to temp
-				else {
-					if (bitCounter == 10) microtime += bitWindow>>2;	//convert timestamp at stop bit to normal 0 bit
-					rawData[backUP] <<= 1;
-					rawData[backUP] |= bitWindow > deltaTime + (bitWindow>>1 & -!(rawData[backUP] & 2));	//add 1/2 bitWindow if previous bit was 1 (for normalisation)
+				else if (BitCounter == 6) rawTemp[ByteNr][backUP] = ByteNr = 0;
+				else if (BitCounter == 10) {			//after stop bit
+					ByteTime = microtime - ByteTime;
+					rawTemp[ByteNr = 1][backUP] = 0;
 				}
+				rawTemp[ByteNr][backUP] <<= 1;      
+				if (deltaTime > bitWindow);				//Logic 0
+				else if (rawTemp[ByteNr][backUP] & 2 || deltaTime < bitWindow - (range >> (BitCounter==11))) rawTemp[ByteNr][backUP] |= 1;  //Logic 1
 				deltaTime = microtime;
 			}
 		}
 		
-		bool tempCheck(uint16_t &rawTemp) {					
-			if (rawTemp >> 14 == 2 && ~rawTemp & 512) {				//check for prefix "10" and stop bit
-				bool parity = true;
-				for (byte i=0; i<9; ++i) parity ^= rawTemp & 1 << i;
-				if (parity) for (byte i=10; i<14; ++i) parity ^= rawTemp & 1 << i;
-				return parity;
-			}
-			return false;
-		}
-		
-		int _sensor;
+		static int isrPin;
 		bool _core;
+	
+		int _Sensortype;
+		static volatile byte BitCounter;
+		static volatile unsigned int ByteTime;
+		static volatile uint16_t rawTemp[2][2];
 		static byte bitWindow;
-		static volatile byte bitCounter;
+		static const byte range = 62;
 		static volatile bool backUP;
-		static volatile uint16_t rawData[2];
-		static volatile byte heartbeat;
+		static volatile unsigned int lastISR;
 };
 
 template<uint8_t pin>
-byte ZACwire<pin>::bitWindow;
+volatile byte ZACwire<pin>::BitCounter = 20;
 template<uint8_t pin>
-volatile byte ZACwire<pin>::bitCounter;
+volatile unsigned int ZACwire<pin>::ByteTime;
 template<uint8_t pin>
 volatile bool ZACwire<pin>::backUP;
 template<uint8_t pin>
-volatile uint16_t ZACwire<pin>::rawData[2];
+volatile uint16_t ZACwire<pin>::rawTemp[2][2];
 template<uint8_t pin>
-volatile byte ZACwire<pin>::heartbeat;
+int ZACwire<pin>::isrPin;
+template<uint8_t pin>
+byte ZACwire<pin>::bitWindow;
+template<uint8_t pin>
+volatile unsigned int ZACwire<pin>::lastISR;
 
 #ifdef ARDUINO_ESP8266_RELEASE_
 void tsicIsrHandler(uint8_t gpio) {
